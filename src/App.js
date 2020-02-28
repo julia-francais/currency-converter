9b468d0aea83ea27faafdb77a91db1af0c5f7ef7
Julia Francais <51647105+julia-francais@users.noreply.github.com>
2020-02-28 15:52:00 +0100

    logic finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import "./styles.css";
 
-class App extends Component {
+const App = () => (
+  <div className="App">
+    <Amount>
+      {amount => (
+        <div>
+          <h4> My currency converter </h4>
+          <Euro amount={amount} />
+          <Pound amount={amount} />
+        </div>
+      )}
+    </Amount>
+  </div>
+);
+
+class Amount extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,41 +23,29 @@ class App extends Component {
     };
   }
   onIncrement = () => {
-    this.setState(state => ({ amount: state.amount + 1 }));
+    this.setState(state => ({ amount: this.state.amount + 1 }));
   };
 
   onDecrement = () => {
-    this.setState(state => ({ amount: state.amount - 1 }));
+    this.setState(state => ({ amount: this.state.amount - 1 }));
   };
-
   render() {
     return (
-      <div className="App">
-        <Amount
-          amount={this.state.amount}
-          onIncrement={this.onIncrement}
-          onDecrement={this.onDecrement}
-        />
-        <Euro amount={this.state.amount} />
-        <Pound amount={this.state.amount} />
+      <div>
+        <span>US Dollar: {this.state.amount} </span>
+
+        <button type="button" onClick={this.onIncrement}>
+          +
+        </button>
+
+        <button type="button" onClick={this.onDecrement}>
+          -
+        </button>
+        {this.props.children(this.state.amount)}
       </div>
     );
   }
 }
-
-const Amount = props => (
-  <div>
-    <span>US Dollar: {props.amount} </span>
-
-    <button type="button" onClick={props.onIncrement}>
-      +
-    </button>
-
-    <button type="button" onClick={props.onDecrement}>
-      -
-    </button>
-  </div>
-);
 
 const Euro = ({ amount }) => <p> Euro: {amount * 0.86}</p>;
 
