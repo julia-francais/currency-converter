3e8b6a78f75d1a05069998afbf70606c5fe89be0
Julia Francais <51647105+julia-francais@users.noreply.github.com>
2020-02-28 15:39:15 +0100

    conver currency ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Currency Converter</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: 0
+    };
+  }
+  onIncrement = () => {
+    this.setState(state => ({ amount: state.amount + 1 }));
+  };
+
+  onDecrement = () => {
+    this.setState(state => ({ amount: state.amount - 1 }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Amount
+          amount={this.state.amount}
+          onIncrement={this.onIncrement}
+          onDecrement={this.onDecrement}
+        />
+        <Euro amount={this.state.amount} />
+        <Pound amount={this.state.amount} />
+      </div>
+    );
+  }
 }
+
+const Amount = props => (
+  <div>
+    <span>US Dollar: {props.amount} </span>
+
+    <button type="button" onClick={props.onIncrement}>
+      +
+    </button>
+
+    <button type="button" onClick={props.onDecrement}>
+      -
+    </button>
+  </div>
+);
+
+const Euro = ({ amount }) => <p> Euro: {amount * 0.86}</p>;
+
+const Pound = ({ amount }) => <p> Pound: {amount * 0.76}</p>;
+
+export default App;
